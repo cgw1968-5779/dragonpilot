b04eb3b22f94bbca300b11f8184fdcdd70246ee3
cgw1968-5779 <58063525+cgw1968-5779@users.noreply.github.com>
2021-07-15 10:41:25 +0800

    Update longitudinal_planner.py

--- selfdrive/controls/lib/longitudinal_planner.py
+++ selfdrive/controls/lib/longitudinal_planner.py
@@ -24,7 +24,7 @@ AWARENESS_DECEL = -0.2     # car smoothly decel at .2m/s^2 when user is distract
 
 # lookup tables VS speed to determine min and max accels in cruise
 # make sure these accelerations are smaller than mpc limits
-_A_CRUISE_MIN_V = [-1.2, -.8, -.67, -.5, -.30]
+_A_CRUISE_MIN_V = [-1.1, -.85, -.6, -.5, -.30]
 _A_CRUISE_MIN_BP = [  0.,  5.,  10., 20.,  40.]
 
 # need fast accel at very low speed for stop and go
@@ -39,7 +39,7 @@ _A_TOTAL_MAX_BP = [20., 40.]
 
 # dp
 DP_FOLLOWING_DIST = {
-  0: 0.95,
+  0: 0.9,
   1: 1.433,
   2: 1.824,
 }
